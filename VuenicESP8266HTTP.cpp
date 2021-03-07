8ad91e15b3e7c9977913677a72807def90dcdf6c
webtoor <fityanalimunshi@gmail.com>
2021-03-07 22:22:10 +0700

    create func wifiConnect

--- VuenicESP8266HTTP.cpp
+++ VuenicESP8266HTTP.cpp
@@ -3,3 +3,39 @@
 VuenicESP8266HTTP::VuenicESP8266HTTP(String apiKey) {
     _apiKey = apiKey;
 }
+
+bool VuenicESP8266HTTP::wifiConnection(String wifiSSID, String wifiPassword) {
+    char wifiSSIDChar[wifiSSID.length()+1];
+    char wifiPasswordChar[wifiPassword.length()+1];
+
+    wifiSSID.toCharArray(wifiSSIDChar, wifiSSID.length()+1);
+    wifiPassword.toCharArray(wifiPasswordChar, wifiPassword.length()+1);
+
+    _wifiSSID = wifiSSIDChar;
+    _wifiPass = wifiPasswordChar;
+
+    WiFi.begin(_wifiSSID, _wifiPass);
+    Serial.println("Connecting to " + wifiSSID + "...");
+
+    int counter = 0;
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println(".");
+        counter++;
+        if(counter >= 10) {
+            counter = 0;
+            Serial.println("Could not connect to " + wifiSSID + "! Retrying...");
+        }
+    }
+
+    Serial.println("WiFi Connected!");
+    Serial.println("IP Address: " + ipToString(WiFi.localIP()));
+    return true;
+}
+
+String VuenicESP8266HTTP::ipToString(IPAddress ip){
+    String s="";
+    for (int i=0; i<4; i++)
+        s += i  ? "." + String(ip[i]) : String(ip[i]);
+    return s;
+}
--- VuenicESP8266HTTP.h
+++ VuenicESP8266HTTP.h
@@ -2,11 +2,21 @@
 #define Vuenic8266HTTP_H
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
 
 class VuenicESP8266HTTP
 {
     public:
       VuenicESP8266HTTP(String apiKey);
+      void begin();
+
+      bool wifiConnection(String SSID, String wifiPassword);
+      
+    private:
+      String _apiKey;
+      char* _wifiSSID;
+      char* _wifiPass;
+      String ipToString(IPAddress ip);
 };
 
 #endif
